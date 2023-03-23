9941eab99781d4f0014e659c3337e435bff523c0
bogdanTeut <bogdan_teut@yahoo.com>
2023-03-23 18:21:29 +0000

    [BogdanT] Refactored code in order to use map.

--- src/main/kotlin/com/book/orders/OrderBook.kt
+++ src/main/kotlin/com/book/orders/OrderBook.kt
@@ -3,24 +3,19 @@ package com.book.orders
 data class Order(val id: Long, val price: Double, val side: Char, val size: Long)
 
 class OrderBook {
-  private val orders = mutableListOf<Order>()
+  private val ordersMap = mutableMapOf<Long, Order>()
 
   fun add(order: Order) {
-    orders += order
+    ordersMap[order.id] = order
   }
 
-  fun fetchAll(): List<Order> = orders
+  fun fetchAll(): List<Order> = ordersMap.values.toList()
   fun remove(orderId: Long) {
-    orders.removeAll { it.id == orderId }
+    ordersMap.remove(orderId)
   }
 
   fun update(orderId: Long, size: Long) {
-    val existingOrder = orders.find { it.id == orderId }
-    val newOrder = existingOrder?.copy(size = size)
+    ordersMap.computeIfPresent(orderId) { _, order -> order.copy(size = size) }
 
-    newOrder?.let {
-      remove(orderId)
-      add(newOrder)
-    }
   }
 }
