6308cb5f2f20e58cb8ebc5da8a6927cd9afa10af
bogdanTeut <bogdan_teut@yahoo.com>
2023-03-24 10:51:53 +0000

    [BogdanT] Refactoring, pulling out common logic for both total size and price logic.

--- src/main/kotlin/com/book/orders/OrderBook.kt
+++ src/main/kotlin/com/book/orders/OrderBook.kt
@@ -1,6 +1,10 @@
 package com.book.orders
 
+import kotlin.collections.Map.Entry
+
 data class Order(val id: Long, val price: Double, val side: Char, val size: Long)
+
+data class PriceLevel(val price: Double, val orders: List<Order>)
 
 class OrderBook {
   private val ordersMap = mutableMapOf<Long, Order>()
@@ -18,20 +22,32 @@ class OrderBook {
     ordersMap.computeIfPresent(orderId) { _, order -> order.copy(size = size) }
   }
 
-  fun price(side: Char, level: Int): Double? {
-    require(level > 0) { "Level must be positive value." }
+  private val mapperToPriceLevel: (Entry<Double, List<Order>>) -> PriceLevel = { (price, orders) -> PriceLevel(price, orders) }
 
+  private fun sortedByPriceLevels(side: Char): List<PriceLevel> {
     val (bids, offers) = ordersMap.values
       .partition { it.side == 'B' }
 
     val bidsByPriceMap = bids.groupBy { it.price }
     val offersByPriceMap = offers.groupBy { it.price }
 
-   return if (side == 'B') bidsByPriceMap.keys.sortedDescending().getOrNull(level-1)
-   else offersByPriceMap.keys.sorted().getOrNull(level-1)
+    return if (side == 'B') bidsByPriceMap.entries.sortedByDescending { it.key }.map(mapperToPriceLevel)
+    else offersByPriceMap.entries.sortedBy { it.key }.map(mapperToPriceLevel)
   }
 
-  fun totalSize(side: Char, level: Int): Int {
-    TODO("Not yet implemented")
+  fun price(side: Char, level: Int): Double? {
+    require(level > 0) { "Level must be positive value." }
+
+    val priceLevels = sortedByPriceLevels(side)
+
+    return priceLevels.getOrNull(level-1)?.price
+  }
+
+  fun totalSize(side: Char, level: Int): Long? {
+    require(level > 0) { "Level must be positive value." }
+
+    val levels = sortedByPriceLevels(side)
+
+    return levels.getOrNull(level-1)?.orders?.sumOf { it.size }
   }
 }
